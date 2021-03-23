52b5d8408e1efb0b6d3f2beaee0a0e7283f50601
ste <foalestephen@gmail.com>
2021-03-23 23:18:42 +0000

    second commit

---
+++ draggable.js
@@ -0,0 +1,66 @@
+// Click and Drag an object
+// Daniel Shiffman <http://www.shiffman.net>
+
+class Draggable {
+  constructor(str, x, y, w, h) {
+    this.dragging = false; // Is the object being dragged?
+    this.rollover = false; // Is the mouse over the ellipse?
+    this.str = str;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
+
+  over() {
+    // Is mouse over object
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      this.rollover = true;
+    } else {
+      this.rollover = false;
+    }
+  }
+
+  update(baf) {
+    // Adjust location if being dragged
+    if (this.dragging) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    }
+    if (baf){
+      this.str = baf;
+    }
+  }
+
+  show(str,val) {
+    stroke(0);
+    // Different fill based on state
+    if (this.dragging) {
+      fill(50);
+    } else if (this.rollover) {
+      fill(100);
+    } else {
+      fill(175, 200);
+    }
+    fill(colorPicker.color());
+    text(this.str, this.x, this.y, this.w, this.h);
+    //textSize(val)
+  }
+
+  pressed() {
+    // Did I click on the rectangle?
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      this.dragging = true;
+      // If so, keep track of relative location of click to corner of rectangle
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
+    }
+  }
+
+  released() {
+    // Quit dragging
+    this.dragging = false;
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,155 @@
+let companyName; 
+let beforeandafter;
+let baf = "Before";
+let before = "Before";
+let after = "After";
+let colorPicker;
+let button;
+let button2;
+let cwidth = 480;
+let cheight = 270;        
+let img; 
+let img2; 
+let Aimg;
+let smaller;
+let input; 
+let input2;
+let nextImage = false;
+
+let slider;
+let buffer; 
+
+
+function preload() {
+//   img = loadImage("before.jpg");
+// img2= loadImage("after.jpg");  
+  
+  //load Gif
+  smaller = loadImage('smallr.gif');
+  
+}
+
+
+function setup() {         
+  let c = createCanvas(cwidth, cheight)
+  buffer = createGraphics(1920,1080)
+  frameRate(30)
+  // c.drop(gotFile);
+    // input before and after images
+  input = createFileInput(handleFile);
+  input.position(0, height +200);
+  input2 = createFileInput(handleFile2);
+  input2.position(200, height +200);
+  
+//create button to reset animation/frame rate
+  button = createButton('Replay');
+  button.position(0,height + 100);
+  button.mouseClicked(clicked);
+  
+  //create button to record
+
+
+  //create slider for text size
+  slider = createSlider(0, 80, 50);
+  slider.position(0, height + 50);
+  
+  //create text fields
+  
+  //create dragable objects with font and color 
+  let cnt = "Pixie Dust";
+  let cntWidth = textWidth(cnt)
+  companyName = new Draggable(cnt, 10, 10, 300, 200);
+  beforeandafter = new Draggable(baf, 10, 200, 200, 200);
+  textFont("Dancing Script")
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker.position(0, height + 5);  
+  
+
+}
+
+
+
+function clicked() {
+  console.log("button pressed")
+  frameCount = 0; 
+  smaller.reset();
+  nextImage = false;
+}
+
+       
+function draw() {
+           
+ buffer.background(255)
+
+  let val = slider.value();
+  textSize(val);       
+   
+  if (img ) { 
+      buffer.image(img, 0, 0, 1920, 1080);    
+    
+    if (frameCount == 60) {
+        nextImage = true; 
+      }
+      
+    if(nextImage){
+        buffer.image(img2, 0, 0, 1920, 1080);
+      }
+ 
+  }
+
+  let frame = smaller.getCurrentFrame();
+         
+      if (frameCount <= 120){             
+            buffer.image(smaller, 0, 0, 1920, 1080);                  
+            text(frameCount, width / 2, height / 2)
+          } 
+  image(buffer, 0, 0, cwidth, cheight);
+          
+if (frameCount >= 60){
+   baf = after; 
+  }else{
+    baf = before;
+}
+  companyName.over();
+  companyName.update();
+  let company = companyName.show();
+  buffer.company;
+  beforeandafter.over();
+  beforeandafter.update(baf);
+ let bef = beforeandafter.show(); 
+  buffer.bef;
+  
+
+}
+
+function mousePressed() {
+  companyName.pressed();
+  beforeandafter.pressed();
+
+}
+
+function mouseReleased() {
+  companyName.released();
+  beforeandafter.released();
+}
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = loadImage(file.data, '');
+    
+    //img.hide();
+    
+  }
+}
+function handleFile2(file) {
+  print(file);
+  if (file.type === 'image') {
+    img2 = loadImage(file.data, '');
+    //img2.hide();
+    
+  } else {
+    img2 = null;
+  }
+}
+                               
